48d0d64aaadde6823da91e71aadf8d8dc0e363e5
Stef Smeets <stef.smeets@mmk.su.se>
2018-01-25 16:56:51 +0100

    Add basic scanning functionality

--- interface.py
+++ interface.py
@@ -1,97 +1,23 @@
 from Tkinter import *
 from ttk import *
 import threading
-#import pyaudio
-#from pyaudio import PyAudio
 
-import sounddevice as sd
-
+from beam_control import BeamCtrl
 import time
-from math import ceil
 import numpy as np
 
-#import matplotlib.pyplot as plt
-
-devices = {
-    'ch12': 'ADAT 1 (1+2) (Fireface ADAT 1 (1+2))',
-    'ch34': 'ADAT 1 (3+4) (Fireface ADAT 1 (3+4))',
-    'ch56': 'ADAT 1 (5+6) (Fireface ADAT 1 (5+6))',
-    'ch78': 'ADAT 1 (7+8) (Fireface ADAT 1 (7+8))',
-    'all':  'ASIO Fireface USB'
-}
-
-
-class BeamCtrl(object):
-    """docstring for BeamCtrl"""
-    def __init__(self, 
-        device=None,          # sounddevice to use, set to `None` to use default
-        fs=44100,             # sampling rate, Hz, must be integer
-        duration=1.0,         # in seconds, may be float
-        global_volume=1.0,    # range [0.0, 1.0]
-        n_channels=2,         # number of channels
-        mapping=(1,2),        # mapping of the channels
-        chunksize=1024,       # number of sampling points per channel
-        **kwargs
-        ): 
-        super(BeamCtrl, self).__init__()
-        
-        sd.default.device  = device
-
-        self.fs            = fs           
-        self.duration      = duration     
-        self.global_volume = global_volume
-        self.n_channels    = n_channels   
-        self.mapping       = mapping
-        self.chunksize     = chunksize    
- 
-        length             = ceil(fs*duration*n_channels)
-        self.signal_data   = np.empty(int(length), dtype=np.float32).reshape(-1, n_channels)
-        self.x             = np.arange(fs*duration) / float(fs)  # x-axis
-
-    def get_signal(self, channel_data):
-        n_channels = self.n_channels
-        global_volume = self.global_volume
-
-        signal_data = self.signal_data
-        x = self.x
-        
-        # multichannel should be interweaved like this: L1R1L2R2L3R3L4R4 (8 channels)
-        # https://zach.se/generate-audio-with-python/
-        
-        for channel in range(n_channels):
-            # generate sine
-            #freq = channel_data[channel]
-            #signal_data.reshape(-1)[channel::n_channels] = (np.sin(2*np.pi*freq*x)).astype(np.float32) * global_volume
-
-            # flat signal
-            volume = channel_data[channel]
-            signal_data.reshape(-1)[channel::n_channels] = volume * global_volume
-
-        return signal_data
-
-    def update(self, channel_data):
-        self.signal = self.get_signal(channel_data)
-
-        # print "signal updated"
-
-    def start_stream(self):
-        print "start stream"
-        sd.play(self.signal_data, self.fs, mapping=self.mapping, loop=True)
-
-    def stop_stream(self):
-        print "stop stream"
-        sd.stop()
+from IPython import embed
 
 
 class BeamCtrlFrame(object, LabelFrame):
     """docstring for BeamCtrlFrame"""
-    def __init__(self, parent, settings):
+    def __init__(self, parent, settings, beam_ctrl):
         LabelFrame.__init__(self, parent, text="Fine beam control")
         self.parent = parent
 
+        self.beam_ctrl = beam_ctrl
         self.channels = settings["channels"]
         self.init_vars()
-        self.beam_ctrl = BeamCtrl(**settings)
 
         frame = Frame(self)
 
@@ -102,20 +28,30 @@ class BeamCtrlFrame(object, LabelFrame):
         self.b_reset.grid(row=5, column=1, sticky="W")
 
         for i, channel in enumerate(self.channels):
-            row = 10+i
-            Label(frame, text=channel["name"], width=20).grid(row=row, column=0, sticky="W")
-            e = Entry(frame, width=10, textvariable=channel["var"])
-            e.grid(row=row, column=1, sticky="W", padx=5)
-    
-            slider = Scale(frame, variable=channel["var"], from_=-100, to=100, orient=HORIZONTAL, command=self.update_channels)
-            slider.grid(row=row, column=2, columnspan=2, sticky="EW")
+            self.make_slider(frame, channel["var"], channel["name"], 10+i, self.update_channels, -100, 100)
+
+        Separator(frame, orient=HORIZONTAL).grid(row=100, columnspan=3, sticky="ew", pady=10)
+
+        self.make_slider(frame, self.var_damping, "Damping factor", 101, self.update_channels, 0, 100)
 
         frame.pack(side="top", fill="x", padx=10, pady=10)
         frame.columnconfigure(2, weight=1)
 
+    def make_slider(self, frame, var, label, row, command, minval, maxval):
+         Label(frame, text=label, width=20).grid(row=row, column=0, sticky="W")
+         e = Spinbox(frame, width=10, textvariable=var, from_=minval, to=maxval)
+         e.grid(row=row, column=1, sticky="W", padx=5)
+    
+         slider = Scale(frame, variable=var, from_=minval, to=maxval, orient=HORIZONTAL, command=command)
+         slider.grid(row=row, column=2, columnspan=2, sticky="EW")
+
     def init_vars(self):
         for channel in self.channels:
-            channel["var"] = channel["var"](channel["default"])
+            channel["var"] = IntVar(value=channel["default"])
+            channel["var"].trace("w", self.update_channels)
+
+        self.var_damping = IntVar(value=100)
+        self.var_damping.trace("w", self.update_channels)
 
         self.var_toggle_beam = BooleanVar(value=False)
 
@@ -136,15 +72,23 @@ class BeamCtrlFrame(object, LabelFrame):
         self.triggerEvent = trigger
         self.q = q
 
-    def update_channels(self, event=None):
-        channel_data = []
-        for channel in self.channels:
-            var = channel["var"]
-            val = var.get()
-            var.set(val)
-            channel_data.append(val / 100.0)
-        # print "\nupdated", channel_data
+    def update_channels(self, *args):
+        # print "\nupdated", time.clock()
+        try:
+            channel_data = self.get_channel_data()
+        except ValueError as e:
+            return
         self.beam_ctrl.update(channel_data)
+
+    def get_channel_data(self):
+        channel_data = []
+        damping = self.var_damping.get() / 100.0
+
+        for channel in self.channels:
+            val = channel["var"].get()
+            channel_data.append(damping*val / 100.0)
+
+        return channel_data
 
 
 settings_fireface = {
@@ -156,14 +100,14 @@ settings_fireface = {
 'chunksize': 1024,
 'mapping': (15,16,17,18,19,20,21,22),
 'channels': (
-    {"name": "BeamShift X?", "var": IntVar, "default": 0},
-    {"name": "BeamShift Y?", "var": IntVar, "default": 0},
-    {"name": "BeamTilt X?", "var": IntVar, "default": 0},
-    {"name": "BeamTilt Y?", "var": IntVar, "default": 0},
-    {"name": "ImageShift? X", "var": IntVar, "default": 0},
-    {"name": "ImageShift? Y", "var": IntVar, "default": 0},
-    {"name": "ImageTilt? X?", "var": IntVar, "default": 0},
-    {"name": "ImageTilt? Y?", "var": IntVar, "default": 0}
+    {"name": "BeamShift X", "var": None, "default": 0},
+    {"name": "BeamShift Y", "var": None, "default": 0},
+    {"name": "BeamTilt X?", "var": None, "default": 0},
+    {"name": "BeamTilt Y?", "var": None, "default": 0},
+    {"name": "ImageShift? X", "var": None, "default": 0},
+    {"name": "ImageShift? Y", "var": None, "default": 0},
+    {"name": "ImageTilt? X?", "var": None, "default": 0},
+    {"name": "ImageTilt? Y?", "var": None, "default": 0}
 )}
 
 settings_testing = {
@@ -175,14 +119,62 @@ settings_testing = {
 'chunksize': 1024,
 'mapping': (1,2),
 'channels': (
-    {"name": "Channel 1", "var": IntVar, "default": 0},
-    {"name": "Channel 2", "var": IntVar, "default": 0}
+    {"name": "Channel 1", "var": None, "default": 0},
+    {"name": "Channel 2", "var": None, "default": 0}
 )}
 
 
+class App(threading.Thread):
+    """docstring for App"""
+    def __init__(self, settings):
+        super(App, self).__init__()
+        #BeamCtrlFrame(root, settings=settings_testing).pack(side="top", fill="both", expand=True)
+        self.settings = settings
+        self.beam_ctrl = BeamCtrl(**settings)
+
+        self.start()
+
+    def run(self):
+        self.root = Tk()
+        BeamCtrlFrame(self.root, settings=self.settings, beam_ctrl=self.beam_ctrl).pack(side="top", fill="both", expand=True)
+        self.root.mainloop()
+
+    def close(self):
+        self.root.quit()
+
 if __name__ == '__main__':
-    root = Tk()
-    BeamCtrlFrame(root, settings=settings_fireface).pack(side="top", fill="both", expand=True)
-    #BeamCtrlFrame(root, settings=settings_testing).pack(side="top", fill="both", expand=True)
-    root.mainloop()
+    app = App(settings=settings_testing)
+
+    embed(banner1="")
+
+    beam = app.beam_ctrl
+
+    # origin (0, 0)
+    raw_input("\n\nPress enter at top left -> origin (0, 0)")
+    o = np.array(beam.channel_data)
+    
+    # topright (x, 0)
+    raw_input("\n\nPress enter at top right -> topright (x, 0)")
+    x = np.array(beam.channel_data) - o
+
+    # botleft (0, y)
+    raw_input("\n\nPress enter at bottom left -> botleft (0, y)")
+    y = np.array(beam.channel_data) - o
+
+    print
+    print
+    print o
+    print x
+    print y
+
+    print
+    for i in range(10):
+        for j in range(10):
+            coord = o + i*x + j*y
+            print "{}: {} -> {}".format(i,j, coord)
+
+            beam.update(coord)
+            time.sleep(0.1)
+
+    app.close()
 
